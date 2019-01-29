34c14e9770172508ca65a0159559d679a05d68d1
Alessandro Bessi <alessandro.bessi@mail.com>
2019-01-29 07:38:46 +0100

    add vector field example

---
+++ field.cpp
@@ -0,0 +1,49 @@
+/*
+g++ field.cpp -o field -lm -lafcpu
+*/
+
+#include <arrayfire.h>
+#include <cstdio>
+#include <math.h>
+
+using namespace af;
+
+const float MINIMUM = -3.0f;
+const float MAXIMUM = 3.0f;
+const float STEP = 0.18f;
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        af::info();
+        af::Window window(1024, 1024, "2D Vector Field");
+        window.grid(1, 2);
+        array range = seq(MINIMUM, MAXIMUM, STEP);
+        array x = tile(range, 1, range.dims(0));
+        array y = tile(range.T(), range.dims(0), 1);
+        x.eval();
+        y.eval();
+        float scale = 5.0f;
+        while (!window.close())
+        {
+            array points = join(1, flat(x), flat(y));
+            array saddle = join(1, flat(x), -1.0f * flat(y));
+            array bvals = cos(scale * (x * x + y * y));
+            array hbowl = join(1, constant(1, x.elements()), flat(bvals));
+            hbowl.eval();
+            window(0, 0).vectorField(points, saddle, "Saddle point");
+            window(0, 1).vectorField(points, hbowl, "Hilly bowl");
+            window.show();
+            scale -= 0.05f;
+            if (scale < -5.0f)
+                scale = 5.0f;
+        }
+    }
+    catch (af::exception &e)
+    {
+        fprintf(stderr, "%s\n", e.what());
+        throw;
+    }
+    return 0;
+}
